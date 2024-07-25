767078bd54f5fd9582da0fbefc6e631e601c8c4f
mikan3rd <ota.hiroki@bizforward.co.jp>
2024-07-26 00:45:07 +0900

    fix: delete comment

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -67,20 +67,9 @@ export const authOptions: NextAuthOptions = {
 
   providers: [
     CredentialsProvider({
-      // The name to display on the sign in form (e.g. 'Sign in with...')
       name: "Guest Login",
-      // The credentials is used to generate a suitable form on the sign in page.
-      // You can specify whatever fields you are expecting to be submitted.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
       credentials: {},
       async authorize() {
-        // You need to provide your own logic here that takes the credentials
-        // submitted and returns either a object representing a user or value
-        // that is false/null if the credentials are invalid.
-        // e.g. return { id: 1, name: 'J Smith', email: 'jsmith@example.com' }
-        // You can also use the `req` object to obtain additional parameters
-        // (i.e., the request IP address)
         const response = await supabase.auth.signInAnonymously();
 
         if (response.error !== null) {
