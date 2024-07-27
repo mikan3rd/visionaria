0de0a9f09dc7b34be0b33f8ddde3af69797c73d1
mikan3rd <ota.hiroki@bizforward.co.jp>
2024-07-27 16:24:28 +0900

    fix: tx

--- src/server/repository/user.ts
+++ src/server/repository/user.ts
@@ -4,8 +4,8 @@ import { users } from "~/server/db/schema";
 type User = typeof users.$inferSelect;
 
 export const createAnonymous = async (): Promise<User> => {
-  return await db.transaction(async (_trx) => {
-    const results = await db
+  return await db.transaction(async (tx) => {
+    const results = await tx
       .insert(users)
       .values({
         name: `Guest ${Date.now()}`,
