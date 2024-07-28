3e2135de97bb050b9c3c22c1adf3d49ce449d8f4
mikan3rd <ota.hiroki@bizforward.co.jp>
2024-07-28 23:26:51 +0900

    chore: add dynamic title

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,5 +1,18 @@
+import { type Metadata, type ResolvingMetadata } from "next";
+
 import { api, HydrateClient } from "~/trpc/server";
 import { Index } from "~/components/pages/user";
+
+export async function generateMetadata(
+  {},
+  parent: ResolvingMetadata,
+): Promise<Metadata> {
+  const user = await api.user.getSelf();
+  const parentTitle = (await parent).title;
+  return {
+    title: `My Page: ${user.name} | ${parentTitle?.absolute}`,
+  };
+}
 
 export default async function Page() {
   void api.user.getSelf.prefetch();
