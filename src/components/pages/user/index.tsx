5044f01437a309196e3a7901dd678fbcb0f950ab
mikan3rd <ota.hiroki@bizforward.co.jp>
2024-07-28 23:07:16 +0900

    fix: style

--- src/components/pages/user/index.tsx
+++ src/components/pages/user/index.tsx
@@ -6,25 +6,27 @@ export const Index = async () => {
   const user = await api.user.getSelf();
 
   return (
-    <div>
-      <div>
-        <div>{user.name}</div>
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
         <div>
-          {user.accounts.map((account) => {
-            return (
-              <div key={account.providerAccountId}>
-                <div>{account.provider}</div>
-              </div>
-            );
-          })}
+          <div>{user.name}</div>
+          <div>
+            {user.accounts.map((account) => {
+              return (
+                <div key={account.providerAccountId}>
+                  <div>{account.provider}</div>
+                </div>
+              );
+            })}
+          </div>
         </div>
+        <Link
+          href="/"
+          className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
+        >
+          Top
+        </Link>
       </div>
-      <Link
-        href="/"
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
-      >
-        Top
-      </Link>
-    </div>
+    </main>
   );
 };
